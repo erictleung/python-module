f7fbc18168aabea36bd577510f9f74dcbe632e21
Eric Leung <leunge@ohsu.edu>
2016-07-02 00:26:21 -0700

    Add Bird and Mammal classes plus test script

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Import classes
+from Animals import Mammals
+from Animals import Birds
+
+# Create mammal
+myMammal = Mammals()
+myMammal.printMembers()
+
+# Create bird
+myBird = Birds()
+myBird.printMembers()
