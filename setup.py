19faf9c92d105c8ca8a6f7edeb3a20ae5efee5e9
Eric Leung <eric@erictleung.com>
2016-11-26 22:24:41 -0800

    setup: do major.minor.patch semantic versioning
    See http://semver.org/

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Animals',
-      version='0.1',
+      version='0.0.1',
       description='Python module to learn module creation and maintenance',
       url='https://github.com/erictleung/python-module',
       author='Eric',
