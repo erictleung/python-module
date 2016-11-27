c0da9298d08c87183bd40b552f88ef0b080a524e
Eric Leung <eric@erictleung.com>
2016-11-26 22:06:44 -0800

    Add setup.py file for setup configuration

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='python-module',
+      version='0.1',
+      description='Python module to learn module creation and maintenance',
+      url='https://github.com/erictleung/python-module',
+      author='Eric',
+      author_email='',
+      license='CC0',
+      packages=['Animals'],
+      zip_safe=False)
